b1714999e7ad35741d6a894d75319c5f398304d7
George Lou <mice2100@163.com>
2019-07-03 03:55:52 +0800

    maintain

--- autolearn.py
+++ autolearn.py
@@ -26,14 +26,18 @@ def subLearn(x, y, duration):
 def findAndLearn(tms, duration):
 	i = 0
 	imgYear = Image.open('year.png')
+	swipe = 0
 	while i<tms:
 		imgScreen = ADB.screencapImg()
 		# imgScreen.save('temp.png')
 		for x, y, w, h in locateAll(imgYear, imgScreen):
 			subLearn(x, y+10, duration)
 			i+=1
+		if i==0 and swipe>1: ## can't find it within 2 pages
+			return
 		if i<tms:
 			ADB.swipe((500, 1500, 500, 500), 1000)
+			swipe += 1
 			UIDelay()
 
 def Learn():
@@ -64,12 +68,12 @@ def Vido():
 	findAndLearn(3, LDelay(True))
 
 def DailyLearn():
-	time.sleep(600*rnd())
+	time.sleep(60*rnd())
 	ADB.unlock()
 	
 	# //launch app
 	ADB.startApp('cn.xuexi.android/com.alibaba.android.rimet.biz.SplashActivity')
-	time.sleep(15)
+	time.sleep(30)
 	
 	Learn()
 	Vido()
@@ -79,7 +83,10 @@ def DailyLearn():
 	ADB.screencap('screenshot.png')
 	mailpng('screenshot.png', ['mice2100@163.com', 'opticmcu@gmail.com'])
 
+	ADB.key("KEYCODE_BACK")
+	UIDelay()
 	ADB.key("KEYCODE_HOME")
+	UIDelay()
 	
 	ADB.lock()
 	
